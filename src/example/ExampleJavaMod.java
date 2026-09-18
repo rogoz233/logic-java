3e6004a8faa69f7a06a4543bd47b464c2c6fcfeb
rogoz233 <gm3570417@gmail.com>
2026-09-18 18:52:01 +0300

    Update dialog titles and button messages in ExampleJavaMod

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -9,32 +9,26 @@ import arc.Core;
 public class ExampleJavaMod extends Mod {
     @Override
     public void init() {
-        // Создаем кнопку в главном меню игры
         Vars.ui.menufrag.addButton("Java -> Mlog", Icon.code, () -> {
             showCompilerDialog();
         });
     }
 
     private void showCompilerDialog() {
-        BaseDialog dialog = new BaseDialog("Транслятор Java в Mlog");
+        BaseDialog dialog = new BaseDialog("Java to Mlog");
         dialog.addCloseButton();
         
-        // Поле ввода кода на телефоне
         var textArea = dialog.cont.field("", text -> {}).size(500, 300).get();
         textArea.setMessageText("int speed = 10;");
 
         dialog.cont.row();
 
-        // Кнопка запуска трансляции
         dialog.cont.button("Скомпилировать", () -> {
             String javaCode = textArea.getText();
-            
-            // ВЫЗОВ ВАШЕГО КЛАССА MlogTranslator
             String mlogResult = MlogTranslator.translate(javaCode);
             
-            // Сохраняем полученный Mlog в буфер обмена телефона
             Core.app.setClipboardText(mlogResult);
-            Vars.ui.showInfoFade("Mlog код скопирован в буфер обмена!");
+            Vars.ui.showInfoFade("Скопировано!");
         }).size(200, 50);
 
         dialog.show();
