10f12d83a096ea9a110295e2133e697acdaa6c3a
rogoz233 <gm3570417@gmail.com>
2026-09-18 18:14:17 +0300

    Refactor logic instruction registration for v8

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,36 @@
 Events.on(ClientLoadEvent, () => {
-    Log.info("Registering custom mlog instructions via JS...");
+    Log.info("[V8 Logic] Initializing custom instructions...");
 
-    // Безопасный способ добавить команду напрямую в глобальный список инструкций игры
-    let instructions = Packages.mindustry.logic.LAssemble.instructions;
-    
-    instructions.put("hello", new Packages.arc.func.Func({
-        get: function(args) {
-            let targetVariable = args.length > 1 ? args[1] : null;
-            
-            return new Packages.mindustry.logic.LExecutor.LInstruction({
-                run: function(exec) {
-                    if (targetVariable != null) {
-                        let variable = exec.getVar(targetVariable);
-                        let value = variable ? variable.numval : 0;
-                        exec.textBuffer.append("Java says: " + value + "\n");
-                    } else {
-                        exec.textBuffer.append("Hello from Logic Extension!\n");
-                    }
+    try {
+        // В v8 парсинг завязан на LogicIO и расширенные методы mlog
+        // Регистрируем кастомное имя инструкции напрямую через глобальный реестр команд игры
+        let logicParser = Packages.mindustry.logic.LAssemble;
+        let LExecutor = Packages.mindustry.logic.LExecutor;
+
+        if (logicParser && logicParser.instructions) {
+            logicParser.instructions.put("hello", new Packages.arc.func.Func({
+                get: function(args) {
+                    let targetVar = args.length > 1 ? args[1] : null;
+                    
+                    return new LExecutor.LInstruction({
+                        run: function(exec) {
+                            if (targetVar != null) {
+                                // Метод получения переменных в v8 был обновлен, обрабатываем безопасно
+                                let variable = exec.getVar(targetVar);
+                                let value = variable ? variable.numval : 0;
+                                exec.textBuffer.append("Java v8 says: " + value + "\n");
+                            } else {
+                                exec.textBuffer.append("Hello from v8 Logic Extension!\n");
+                            }
+                        }
+                    });
                 }
-            });
+            }));
+            Log.info("[V8 Logic] Instruction 'hello' has been successfully injected!");
+        } else {
+            Log.err("[V8 Logic] Critical Error: Cannot find logic instructions registry.");
         }
-    }));
-
-    Log.info("Instruction 'hello' successfully added via JavaScript!");
+    } catch(e) {
+        Log.err("[V8 Logic] Failed to inject instruction: " + e);
+    }
 });
-          
